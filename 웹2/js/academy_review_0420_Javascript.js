20089a2b87a5bebc55dd6d945c66db0c2b29ea72
worud516 <worud516@naver.com>
2021-04-20 10:49:53 +0900

    Javascript

---
+++ 웹2/js/academy_review_0420_Javascript.js
@@ -0,0 +1,143 @@
+// 주어.동사(목적어);
+// 콘솔화면을 깨끗이 지운다.
+console.clear();
+
+console.log(15);
+// console.log(t);
+// 콘솔에 값을 출력한다.
+
+// var 키워드는 변수를 만들때 한!번!만! 사용한다.
+
+var t = "사과"; 
+
+console.log(t);
+
+// 변수의 값은 바뀔 수 있다.
+// x = 3
+
+t = 15;
+
+console.log(t);
+
+var x = 2;
+var y = 5;
+
+console.log(x - y);
+console.log("안녕");
+
+console.clear();
+console.log(15);
+console.log("안녕하세요");
+console.log(1+2);
+
+var hi = 3;
+
+var code_pen = 4;
+
+var test = "안녕하세요";
+
+var dan = 2;
+
+console.log(dan + "단");
+console.log(dan + " X 1 = "+ (dan * 1));
+console.log(dan + " X 2 = "+ (dan * 2));
+console.log(dan + " X 3 = "+ (dan * 3));
+console.log(dan + " X 4 = "+ (dan * 4));
+console.log(dan + " X 5 = "+ (dan * 5));
+console.log(dan + " X 6 = "+ (dan * 6));
+console.log(dan + " X 7 = "+ (dan * 7));
+console.log(dan + " X 8 = "+ (dan * 8));
+console.log(dan + " X 9 = "+ (dan * 9));
+
+var dan = 3;
+
+console.log(dan + "단");
+console.log(dan + " X 1 = "+ (dan * 1));
+console.log(dan + " X 2 = "+ (dan * 2));
+console.log(dan + " X 3 = "+ (dan * 3));
+console.log(dan + " X 4 = "+ (dan * 4));
+console.log(dan + " X 5 = "+ (dan * 5));
+console.log(dan + " X 6 = "+ (dan * 6));
+console.log(dan + " X 7 = "+ (dan * 7));
+console.log(dan + " X 8 = "+ (dan * 8));
+console.log(dan + " X 9 = "+ (dan * 9));
+
+var dan = 4;
+
+console.log(dan + "단");
+console.log(dan + " X 1 = "+ (dan * 1));
+console.log(dan + " X 2 = "+ (dan * 2));
+console.log(dan + " X 3 = "+ (dan * 3));
+console.log(dan + " X 4 = "+ (dan * 4));
+console.log(dan + " X 5 = "+ (dan * 5));
+console.log(dan + " X 6 = "+ (dan * 6));
+console.log(dan + " X 7 = "+ (dan * 7));
+console.log(dan + " X 8 = "+ (dan * 8));
+console.log(dan + " X 9 = "+ (dan * 9));
+
+var dan = 5;
+
+console.log(dan + "단");
+console.log(dan + " X 1 = "+ (dan * 1));
+console.log(dan + " X 2 = "+ (dan * 2));
+console.log(dan + " X 3 = "+ (dan * 3));
+console.log(dan + " X 4 = "+ (dan * 4));
+console.log(dan + " X 5 = "+ (dan * 5));
+console.log(dan + " X 6 = "+ (dan * 6));
+console.log(dan + " X 7 = "+ (dan * 7));
+console.log(dan + " X 8 = "+ (dan * 8));
+console.log(dan + " X 9 = "+ (dan * 9));
+
+var dan = 6;
+
+console.log(dan + "단");
+console.log(dan + " X 1 = "+ (dan * 1));
+console.log(dan + " X 2 = "+ (dan * 2));
+console.log(dan + " X 3 = "+ (dan * 3));
+console.log(dan + " X 4 = "+ (dan * 4));
+console.log(dan + " X 5 = "+ (dan * 5));
+console.log(dan + " X 6 = "+ (dan * 6));
+console.log(dan + " X 7 = "+ (dan * 7));
+console.log(dan + " X 8 = "+ (dan * 8));
+console.log(dan + " X 9 = "+ (dan * 9));
+
+var dan = 7;
+
+console.log(dan + "단");
+console.log(dan + " X 1 = "+ (dan * 1));
+console.log(dan + " X 2 = "+ (dan * 2));
+console.log(dan + " X 3 = "+ (dan * 3));
+console.log(dan + " X 4 = "+ (dan * 4));
+console.log(dan + " X 5 = "+ (dan * 5));
+console.log(dan + " X 6 = "+ (dan * 6));
+console.log(dan + " X 7 = "+ (dan * 7));
+console.log(dan + " X 8 = "+ (dan * 8));
+console.log(dan + " X 9 = "+ (dan * 9));
+
+var dan = 8;
+
+console.log(dan + "단");
+console.log(dan + " X 1 = "+ (dan * 1));
+console.log(dan + " X 2 = "+ (dan * 2));
+console.log(dan + " X 3 = "+ (dan * 3));
+console.log(dan + " X 4 = "+ (dan * 4));
+console.log(dan + " X 5 = "+ (dan * 5));
+console.log(dan + " X 6 = "+ (dan * 6));
+console.log(dan + " X 7 = "+ (dan * 7));
+console.log(dan + " X 8 = "+ (dan * 8));
+console.log(dan + " X 9 = "+ (dan * 9));
+
+var dan = 9;
+
+console.log(dan + "단");
+console.log(dan + " X 1 = "+ (dan * 1));
+console.log(dan + " X 2 = "+ (dan * 2));
+console.log(dan + " X 3 = "+ (dan * 3));
+console.log(dan + " X 4 = "+ (dan * 4));
+console.log(dan + " X 5 = "+ (dan * 5));
+console.log(dan + " X 6 = "+ (dan * 6));
+console.log(dan + " X 7 = "+ (dan * 7));
+console.log(dan + " X 8 = "+ (dan * 8));
+console.log(dan + " X 9 = "+ (dan * 9));
+
+document.write("<h1> 111 </h1>");
