d733a316ad233969a38a1a252eb4211411233fb5
worud516 <worud516@naver.com>
2021-05-14 10:42:16 +0900

    slide2 auto append

--- Web3/js/academy_review_0513_Slides_2.js
+++ Web3/js/academy_review_0513_Slides_2.js
@@ -2,9 +2,21 @@ $(document).ready(function () {
 
     var i = 0;
 
+    var box_length = $(".box-wrap > .box-list > .box").length;
+
+    $(".box-list").css({
+        "width": 900 * box_length + "px",
+    })
+
+
+    for (var i = 0; i < box_length; i++) {
+        $(".dots-wrap").append("<li class='dots cell'></li>");
+    };
+
+
     setInterval(function () {
 
-        if (i > 3) {
+        if (i > box_length - 1) {
             i = 0;
         }
 
@@ -17,6 +29,6 @@ $(document).ready(function () {
 
         i++;
 
-    }, 1500);
+    }, 1000);
 
 });
