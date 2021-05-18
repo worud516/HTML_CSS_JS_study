ad9512bd77459003f589fe436c440f20e30ca061
worud516 <worud516@naver.com>
2021-05-18 11:53:38 +0900

    slickslideroption

--- Web3/js/academy_review_0517_SlickSlider.js
+++ Web3/js/academy_review_0517_SlickSlider.js
@@ -1,3 +1,20 @@
 $(document).ready(function () {
-    $(".slider-wrap").slick();
+    $(".slider-wrap").slick({
+        autoplay: true,
+        autoplaySpeed: 1700,
+        dots: true,
+        //fade: true,   페이드인 페이드 아웃
+        //slidesToShow: 1, 한번에 나오는 개수
+        //slidesToScroll: 1, 몇개씩 넘어갈거냐
+        vertical: true, // 아래에서 위로
+        verticalSwiping: true, //아래에서 위로 드래그시 슬라이드 될수있게 하는거
+    });
+
+    $(".slider-wrap2").slick({
+        autoplay: true,
+        autoplaySpeed: 1700,
+        dots: true,
+    });
+
+
 });
