b500600f92c72467fc63320139bf460886e7ba60
worud516 <worud516@naver.com>
2021-05-20 10:31:07 +0900

    기능 추가 화살표와 닷 변경

--- Web3/js/academy_review_0517_SlickSlider.js
+++ Web3/js/academy_review_0517_SlickSlider.js
@@ -8,7 +8,13 @@ $(document).ready(function () {
         //slidesToScroll: 1, 몇개씩 넘어갈거냐
         vertical: true, // 아래에서 위로
         verticalSwiping: true, //아래에서 위로 드래그시 슬라이드 될수있게 하는거
+        nextArrow: "<div class='btn-right'><img src='https://lh3.googleusercontent.com/proxy/Vp6JB7DeZbZYP_x4jcf-l2oO1ZkCTzq40Y01Knw5J_gEuKzjo4Wsdv_YiYublsstNr-WsCoRzzu_JKd98yjULaaE3zfQzo0-S2lsr0YcFV4_efeKN1oq5IO8Ssmal9uyp3fwQ9hqnILmEUwOzKfNHN5pcFe0y9UF21gt4cH-foWru-Mz9TAw84qSiWTUx7vV1aeiK5CEUYC3fVswcz0woCdA5uFiir-qWJycQ3rHBVRvtbq9lLmQYpdTsYRp4Jgq-RlOoaa5rZN2CTRn5zE-_xcPpFGgIgQr4gofqEpl06eoisTQK6Q14LF_CAiZ1YiweEWR' alt=''></div>",
+        prevArrow: "<<div class='btn-left'><i class='fas fa-arrow-circle-left'></i></div>",
     });
+
+
+
+
 
     $(".slider-wrap2").slick({
         autoplay: true,
