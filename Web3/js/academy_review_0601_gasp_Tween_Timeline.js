f25993a79974c130b474131cd6cabf59aad5617b
worud516 <worud516@naver.com>
2021-06-01 12:17:57 +0900

    gsap timeline

--- Web3/js/academy_review_0601_gasp_Tween_Timeline.js
+++ Web3/js/academy_review_0601_gasp_Tween_Timeline.js
@@ -2,11 +2,24 @@ $(document).ready(function () {
 
     $(".box1").click(function () {
 
-        gsap.from(".box1", {
-            rotation: 27,
-            y: 100,
+        // gsap.from(".box1", {rotation: 27, y: 100, backgroundColor : "blue" , borderRadius : "50", duration: 1});
+
+        var test = gsap.timeline({
+            repeat: 2,
+            repeatDelay: 1,
+            yoyo: true
+        });
+        test.to(".box1", {
+            x: 100,
+            y: 50,
+            duration: 1
+        });
+        test.to(".box1", {
             backgroundColor: "blue",
-            borderRadius: "50",
+            duration: 2
+        });
+        test.to(".box1", {
+            borderRadius: "50px",
             duration: 1
         });
 
