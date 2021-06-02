2caf834b51b91987479f196d5c5b29468e03778a
worud516 <worud516@naver.com>
2021-06-02 10:17:37 +0900

    svg효과 추가

--- Web3/js/academy_review_0602_DrowSVG.js
+++ Web3/js/academy_review_0602_DrowSVG.js
@@ -8,5 +8,19 @@ $(document).ready(function () {
     });
     mySVG.drawsvg('animate');
 
+    var mySVG = $('.test2').drawsvg({
+        duration: 1000, // 한글자 그려지는 시간
+        stagger: 1000, // 다음 글자 그려지는 순서
+        eading: 'swing', // css ease-in out
+        reverse: false // 그려지는 순서 true / false
+    });
+    mySVG.drawsvg('animate');
 
+    var $svg = $('svg').drawsvg({
+        duration: 1000, // 한글자 그려지는 시간
+        stagger: 1000, // 다음 글자 그려지는 순서
+        easing: 'swing', // css ease-in out 등등 속성ㅇ
+        reverse: false, //그려지는 순서 true / false
+    })
+    $svg.drawsvg('animate');
 });
