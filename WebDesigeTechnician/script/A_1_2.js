4b15b77e769a476cb1d97b9324734122d8e935c9
worud516 <worud516@naver.com>
2021-07-28 21:50:18 +0900

    웹디기 연습

--- WebDesigeTechnician/script/A_1_2.js
+++ WebDesigeTechnician/script/A_1_2.js
@@ -19,6 +19,29 @@ $(document).ready(function(){
         });
     },3000);
 
+    $(".contents .notice_gal h3.header1").click(function(){
+        $(".contents .notice_gal h3.l100").removeClass("on");
+        $(".contents .notice_gal ul.gal").removeClass("on");
+        $(".contents .notice_gal h3.header1").addClass("on");
+        $(".contents .notice_gal ul.notice").addClass("on");
+
+    });
+
+    $(".contents .notice_gal h3.l100").click(function(){
+        $(".contents .notice_gal h3.header1").removeClass("on");
+        $(".contents .notice_gal ul.notice").removeClass("on");
+        $(".contents .notice_gal h3.l100").addClass("on");
+        $(".contents .notice_gal ul.gal").addClass("on");
+    });
+
+    $("ul.notice li").eq(0).click(function(){
+        $(".modal").fadeIn();
+    });
+
+    $("button").click(function(){
+        $(".modal").fadeOut();
+    });
+
 
 
 });
