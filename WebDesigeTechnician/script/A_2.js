8964580b5b4724bd88ce82f562e2b2d1380bc017
worud516 <worud516@naver.com>
2021-08-02 19:43:41 +0900

    A_2 헤더 메뉴 구현

--- WebDesigeTechnician/script/A_2.js
+++ WebDesigeTechnician/script/A_2.js
@@ -1,6 +1,18 @@
 $(document).ready(function(){
 
 
+    $("header nav ul.gnb>li").hover(function(){
+        $("header nav .nav_bg").stop().slideDown();
+        $("header nav ul.gnb>li>ul.sub").stop().slideDown();
+    },function(){
+        $("header nav .nav_bg").stop().slideUp();
+        $("header nav ul.gnb>li>ul.sub").stop().slideUp();
+    });
+
+
+
+
+
     var slideNum = 0;
 
     setInterval(function(){
