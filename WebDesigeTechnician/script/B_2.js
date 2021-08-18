c7dab7df54163981e1f7966d56b7371adf2dabad
worud516 <worud516@naver.com>
2021-08-18 22:15:22 +0900

    B_2 대한은행 헤더 완료

--- WebDesigeTechnician/script/B_2.js
+++ WebDesigeTechnician/script/B_2.js
@@ -1,4 +1,12 @@
 $(document).ready(function(){
 
+    $("header nav>ul>li").hover(function(){
+        $("header nav>ul>li>ul").stop().slideDown();
+        $(".navbg").stop().slideDown(); 
+    },function(){
+        $("header nav>ul>li>ul").stop().slideUp();
+        $(".navbg").stop().slideUp();
+    })
+    
 
 });
