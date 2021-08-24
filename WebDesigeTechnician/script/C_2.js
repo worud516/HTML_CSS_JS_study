92cc29eef5f934118d2023cf9f4e210cb1fac5fb
worud516 <worud516@naver.com>
2021-08-24 19:57:42 +0900

    slide 완료

--- WebDesigeTechnician/script/C_2.js
+++ WebDesigeTechnician/script/C_2.js
@@ -7,6 +7,19 @@ $(document).ready(function(){
         $(this).children("ul").stop().slideUp();
     });
 
+    var num = 0;
 
+    $(".wrap > .rightbox > .slide > ul > li").eq(num).siblings().hide();
+
+    setInterval(function(){
+        num++;
+        if( num >= 3){
+            num = 0;
+        }
+
+        $(".wrap > .rightbox > .slide > ul > li").eq(num).siblings().fadeOut(1000);
+        $(".wrap > .rightbox > .slide > ul > li").eq(num).fadeIn(1000);
+
+    },3000);
 
 });
