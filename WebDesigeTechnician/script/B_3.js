ed9ffe324cf54760ca9da93714fbda5b22073539
worud516 <worud516@naver.com>
2021-08-27 17:55:20 +0900

    헤더 작업 완료

--- WebDesigeTechnician/script/B_3.js
+++ WebDesigeTechnician/script/B_3.js
@@ -1,6 +1,10 @@
 $(document).ready(function(){
 
-
+    $(".wrap > header > ul > li").hover(function(){
+        $(this).children("ul").stop().slideDown();
+    },function(){
+        $(this).children("ul").stop().slideUp();
+    });
 
 
 
