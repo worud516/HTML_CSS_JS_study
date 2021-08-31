232431fe0e98d5581e714552cf751c8ad567443d
worud516 <worud516@naver.com>
2021-08-31 16:28:07 +0900

    슬라이드 완료

--- WebDesigeTechnician/script/C_12.js
+++ WebDesigeTechnician/script/C_12.js
@@ -6,4 +6,20 @@ $(document).ready(function(){
         $(this).children("ul").stop().slideUp();
     });
 
+
+
+    var num = 0;
+
+    $(".wrap > .rbox > ul.slide > li").eq(0).siblings().fadeOut(0);
+    $(".wrap > .rbox > ul.slide > li").eq(0).fadeIn(0);
+
+    setInterval(function(){
+        num++;
+        if( num >= 3){
+            num = 0;
+        }
+        $(".wrap > .rbox > ul.slide > li").eq(num).siblings().fadeOut(1000);
+        $(".wrap > .rbox > ul.slide > li").eq(num).fadeIn(1000);
+    },3000);
+
 });
